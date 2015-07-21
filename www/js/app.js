7ca282a2c9cba2f7600ce304c18058a49aa72cd9
vincentw <vw1011@hotmail.com>
2015-07-21 00:49:45 -0700

    Left side menu and basic working routing and ion-nav-view s

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,6 @@
-// Ionic Starter App
+var app = angular.module('starter', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +11,27 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('trails', {
+    url: '/',
+    templateUrl: 'trails.html'
+  })
+
+$stateProvider.state('log', {
+  url: '/log',
+  templateUrl: 'logitems.html'
+    })
+});
+
+
+app.controller('StartController', function($scope, $ionicModal, $ionicSideMenuDelegate) {
+
+  $scope.togglePancake = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+});
