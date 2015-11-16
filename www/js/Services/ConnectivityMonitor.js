06ac515dd7a5500a14203af2af91b7eab066db6b
Vincent Wong <vincentw@live.ca>
2015-11-15 23:53:55 -0800

    #56: show Map only if user connected to internet, otherwise show error message

---
+++ www/js/Services/ConnectivityMonitor.js
@@ -0,0 +1,24 @@
+var app = angular.module('services');
+
+app.factory('ConnectivityMonitor', function ($rootScope, $cordovaNetwork) {
+    return {
+        isOnline: function () {
+
+            if (ionic.Platform.isWebView()) {
+                return $cordovaNetwork.isOnline();
+            } else {
+                return navigator.onLine;
+            }
+
+        },
+        ifOffline: function () {
+
+            if (ionic.Platform.isWebView()) {
+                return !$cordovaNetwork.isOnline();
+            } else {
+                return !navigator.onLine;
+            }
+
+        }
+    }
+});
--- www/js/Services/GoogleMaps.js
+++ www/js/Services/GoogleMaps.js
@@ -1,18 +1,26 @@
 var app = angular.module('services');
 
-app.factory('GoogleMaps', function(){
+app.factory('GoogleMaps', ['$cordovaGeolocation', '$ionicLoading', '$rootScope', '$cordovaNetwork', 'ConnectivityMonitor', function ($cordovaGeolocation, $ionicLoading, $rootScope, $cordovaNetwork, ConnectivityMonitor) {
+
     var apiKey = false;
     var map = null;
 
-    var latLong = new google.maps.LatLng(49.2827, -123.1207); //Lat long of Downtown Vancouver
-    var zoom = 12;
+    var trails = [];
 
-    function initMap(trails){
-        if(trails.length < 1) return;
-        else if(trails.length == 1) {
+    function initMap() {
+
+        var latLong = new google.maps.LatLng(49.2827, -123.1207); //Lat long of Downtown Vancouver
+        var zoom = 12;
+
+        if(!trails || trails.length < 1) {
+            $ionicLoading.show({
+                template: 'An unexpected error occurred.'
+            });
+        }
+        if (trails.length == 1) {
             latLong = new google.maps.LatLng(trails[0].lat, trails[0].long);
         }
-        else {
+        else if (trails.length > 1) {
             console.log("More than 1 trail to mark");
             //do something to calculate latLng on middle and the zoom ratio
             return;
@@ -27,13 +35,58 @@ app.factory('GoogleMaps', function(){
         map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
         //Wait until the map is loaded
-        google.maps.event.addListenerOnce(map, 'idle', function(){
-            loadMarkers(trails);
+        google.maps.event.addListenerOnce(map, 'idle', function () {
+            loadMarkers();
+            enableMap();
         });
     }
 
-    function loadMarkers(trails){
-        for(var i = 0; i < trails.length; i++){
+    function enableMap() {
+        $ionicLoading.hide();
+    }
+
+    function disableMap() {
+        $ionicLoading.show({
+            template: "You must be connected to the Internet to view this map."
+        })
+    }
+
+    function loadGoogleMaps() {
+        $ionicLoading.show({
+            template: 'Loading Google Maps...'
+        });
+
+        //This function will be called once the SDK has been loaded
+        window.mapInit = function () {
+            initMap();
+        };
+
+        //Create a script element to insert Google JS Maps API into the page
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.id = "googleMaps";
+
+        if (apiKey) {
+            script.src = 'http://maps.google.com/maps/api/js?key=' + apiKey + '&sensor=true&callback=mapInit';
+        }
+        else {
+            script.src = 'http://maps.google.com/maps/api/js?sensor=true&callback=mapInit';
+        }
+
+        document.body.appendChild(script);
+    }
+
+    function checkLoaded() {
+        if (typeof google == "undefined" || typeof google.maps == "undefined") {
+            loadGoogleMaps();
+        }
+        else {
+            enableMap();
+        }
+    }
+
+    function loadMarkers() {
+        for (var i = 0; i < trails.length; i++) {
             var trail = trails[i];
             var markerPos = new google.maps.LatLng(trail.lat, trail.long);
 
@@ -43,24 +96,69 @@ app.factory('GoogleMaps', function(){
                 position: markerPos
             });
 
-            var infoWindowContent = "<h4>" +  trail.name + "</h4>";
+            var infoWindowContent = "<h4>" + trail.name + "</h4>";
             addInfoWindow(marker, infoWindowContent, trail);
         }
     }
 
-    function addInfoWindow(marker, message, trail){
+    function addInfoWindow(marker, message, trail) {
         var infoWindow = new google.maps.InfoWindow({
             content: message
         });
 
-        google.maps.event.addListener(marker, 'click', function(){
+        google.maps.event.addListener(marker, 'click', function () {
             infoWindow.open(map, marker);
         });
     }
 
-    return{
-        init: function(trails){
-            initMap(trails);
+    function addConnectivityListeners() {
+        if (ionic.Platform.isWebView()) {
+            //Check if the map is already loaded when user comes online, if not, load it
+            $rootScope.$on('$cordovaNetwork:online', function (event, networkState) {
+                checkLoaded();
+            });
+
+            //Disable the map when the user goes offline
+            $rootScope.$on('$cordovaNetwork:offline', function (event, networkState) {
+                disableMap();
+            });
+        }
+        else {
+            window.addEventListener("online", function (e) {
+                checkLoaded();
+            }, false);
+
+            window.addEventListener("offline", function (e) {
+                disableMap();
+            }, false);
         }
     }
-});
+
+    return {
+        init: function (arrayOfMarkers) {
+            trails = arrayOfMarkers;
+            apiKey = "AIzaSyClUi3fHwITYAL8qhel240O3r3scblMG8g";
+
+            if (typeof google == "undefined" || typeof google.maps == "undefined") {
+                console.warn("Google Maps SDK needs to be loaded");
+
+                disableMap();
+
+                if (ConnectivityMonitor.isOnline()) {
+                    loadGoogleMaps();
+                }
+            }
+            else {
+                if (ConnectivityMonitor.isOnline()) {
+                    initMap();
+                    enableMap();
+                }
+                else {
+                    disableMap();
+                }
+            }
+
+            addConnectivityListeners();
+        }
+    }
+}]);
