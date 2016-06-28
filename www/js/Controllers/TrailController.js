173ea0c5048f7a6d48694a287c0fbebcece5fefe
Vincent Wong <vincentw@live.ca>
2016-06-28 01:01:47 -0700

    #53: Implement ability to share a trail card to native sharing apps on device

--- www/js/Controllers/TrailController.js
+++ www/js/Controllers/TrailController.js
@@ -1,6 +1,6 @@
 var app = angular.module('controllers');
 
-app.controller('TrailController', ['$scope', '$state', '$stateParams', '$ionicLoading', 'TrailsService', 'FavouritesService', '$ionicSideMenuDelegate', function ($scope, $state, $stateParams, $ionicLoading, TrailsService, FavouritesService, $ionicSideMenuDelegate) {
+app.controller('TrailController', ['$scope', '$state', '$stateParams', '$filter', '$ionicLoading', 'TrailsService', 'FavouritesService', '$ionicSideMenuDelegate', function ($scope, $state, $stateParams, $filter, $ionicLoading, TrailsService, FavouritesService, $ionicSideMenuDelegate) {
     $scope.state = $state.current;
     $scope.params = $stateParams;
 
@@ -29,4 +29,35 @@ app.controller('TrailController', ['$scope', '$state', '$stateParams', '$ionicLo
     $scope.$on('$ionicView.enter', function(){
         $ionicSideMenuDelegate.canDragContent(true);
     });
+
+    $scope.shareTrail = function() {
+        var trail = $scope.trail;
+
+        var humanizeDifficultyFilter = $filter('HumanizeDifficulty');
+        var convertDistanceCategoryToStringFilter = $filter('ConvertDistanceCategoryToString');
+        var processElevationToStringFilter = $filter('ProcessElevationToString');
+        var humanizeSeasonFilter = $filter('HumanizeSeason');
+
+        var message =
+            trail.name + "\n" +
+            "Location: " + trail.location + "\n" +
+            "Difficulty: " + humanizeDifficultyFilter(trail.difficulty) + "\n" +
+            "Time: " + trail.time + " hours" + "\n" +
+            convertDistanceCategoryToStringFilter(trail.distanceCategory) + ": " + trail.distance + "km" + "\n" +
+            "Elevation Gain: " + processElevationToStringFilter(trail.elevation) + "\n" +
+            "Season: " + humanizeSeasonFilter(trail.season) + "\n";
+        // message, subject, file, url, [successCallback], [errorCallback]
+        window.plugins.socialsharing.share(
+            message,
+            trail.name,
+            null,
+            "http://maps.google.com/maps?daddr=" + trail.lat + "," + trail.long,
+            null,
+            function (err) {
+                window.plugins.toast.showShortBottom(
+                    "Failed to Share"
+                );
+            }
+        );
+    };
 }]);
