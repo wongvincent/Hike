6f2fb1e22b58c63f630c98fb28253f405f2916ae
Vincent Wong <vincentw@live.ca>
2016-06-27 22:03:44 -0700

    #89: Change colour of status bar -  Attempt 1

--- www/js/app.js
+++ www/js/app.js
@@ -3,13 +3,15 @@ var db = null;
 
 app.run(function ($rootScope, $ionicPlatform, $ionicHistory, $ionicPopup) {
     $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
+        if (window.cordova && StatusBar) {
+            if (ionic.Platform.isAndroid()) {
+                StatusBar.backgroundColorByHexString("#33CD5F");
+            } else {
+                StatusBar.styleBlackTranslucent();
+            }
         }
     });
 
