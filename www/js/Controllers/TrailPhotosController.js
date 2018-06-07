85a758f7c826fafebfa0c2a547f133520946e115
Vincent Wong <vincentw@live.ca>
2018-06-07 01:30:36 -0700

    Click photo opens lightbox

--- www/js/Controllers/TrailPhotosController.js
+++ www/js/Controllers/TrailPhotosController.js
@@ -1,6 +1,8 @@
 var app = angular.module('controllers');
 
-app.controller('TrailPhotosController', ['$rootScope', '$scope', 'GooglePlacesService', '$ionicLoading', 'ConnectivityMonitor', function($rootScope, $scope, GooglePlacesService, $ionicLoading, ConnectivityMonitor) {
+app.controller('TrailPhotosController', ['$rootScope', '$scope', 'GooglePlacesService', '$ionicLoading', '$ionicBackdrop', '$ionicModal', '$ionicSlideBoxDelegate', '$ionicScrollDelegate', 'ConnectivityMonitor', function($rootScope, $scope, GooglePlacesService, $ionicLoading, $ionicBackdrop, $ionicModal, $ionicSlideBoxDelegate, $ionicScrollDelegate, ConnectivityMonitor) {
+  $scope.zoomMin = 1;
+
   $scope.$on('$ionicView.enter', function() {
     if (analytics) analytics.trackView('Trail - Photos');
   });
@@ -82,4 +84,35 @@ app.controller('TrailPhotosController', ['$rootScope', '$scope', 'GooglePlacesSe
     $scope.trail.photos = [];
     $scope.noConnectionAlert('An internet connection is required to load trail photos.');
   }
+
+  $scope.openPhotosModal = function(index) {
+    $scope.activeSlide = index;
+    $ionicModal.fromTemplateUrl('views/trail/photosModal.html', {
+      scope: $scope,
+    }).then(function(modal) {
+      $scope.photosModal = modal;
+      $scope.photosModal.show();
+      // Remove click-block, otherwise when photos-modal closes,
+      // click events are briefly blocked
+      const clickBlockElements = angular.element(document.getElementsByClassName('click-block'));
+      if (clickBlockElements.length > 0) {
+        clickBlockElements[0].parentNode.removeChild(clickBlockElements[0]);
+      }
+    });
+  };
+
+  $scope.closePhotosModal = function() {
+    $scope.photosModal.hide();
+    $scope.photosModal.remove();
+  };
+
+  $scope.updateSlideStatus = function(slide) {
+    var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide).getScrollPosition().zoom;
+    if (zoomFactor == $scope.zoomMin) {
+      $ionicSlideBoxDelegate.enableSlide(true);
+    } else {
+      $ionicSlideBoxDelegate.enableSlide(false);
+    }
+  };
+
 }]);
